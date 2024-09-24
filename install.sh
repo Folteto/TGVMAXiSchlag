cae4d036df20f62fe6c62943689dc5e791ed64bb
YohanBeattie <68608803+YohanBeattie@users.noreply.github.com>
2024-09-24 12:50:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ eval "$(register-python-argcomplete main.py)"
 chmod +x main.py
 zshrc_file="$HOME/.zshrc"
 
-if ! grep -q "compinit" "$zshrc_file"; then
+if ! grep -q "autoload -U compinit; compinit" "$zshrc_file"; then
   echo "La commande 'compinit' n'est pas présente dans le fichier .zshrc. Ajout de la commande..."
-  echo "autoload -Uz compinit" >> "$zshrc_file"
-  echo "compinit" >> "$zshrc_file"
+  echo "autoload -U compinit; compinit" >> "$zshrc_file"
+  else 
+  echo "nope"
 fi
