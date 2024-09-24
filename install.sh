0874e7da0ea3cd2f7c809ca19e2180f9b7317baf
YohanBeattie <68608803+YohanBeattie@users.noreply.github.com>
2024-09-24 12:42:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
 sudo activate-global-python-argcomplete
-pipx install `cat requirements.txt"
+sudo apt install python-argparse
 eval "$(register-python-argcomplete main.py)"
 chmod +x main.py
+zshrc_file="$HOME/.zshrc"
+
+if ! grep -q "compinit" "$zshrc_file"; then
+  echo "La commande 'compinit' n'est pas présente dans le fichier .zshrc. Ajout de la commande..."
+  echo "autoload -Uz compinit" >> "$zshrc_file"
+  echo "compinit" >> "$zshrc_file"
+fi
