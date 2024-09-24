189c51ffa21b4dc8e57b55860cd71c2074e22dc6
YohanBeattie <68608803+YohanBeattie@users.noreply.github.com>
2024-09-24 12:01:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 sudo activate-global-python-argcomplete
-git clone https://github.com/Folteto/TGVMAXiSchlag.git
-cd TGVMAXiSchlag
 pipx install -r requirement.txt
 eval "$(register-python-argcomplete main.py)"
 chmod +x main.py
